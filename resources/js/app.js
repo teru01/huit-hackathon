4ec90a70aab65baa103eb3bf350ae59fadea4e4e
teruya <abas.trace.on@gmail.com>
2019-02-02 18:22:20 +0900

    add components

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,9 @@
 import Vue from 'vue'
+import router from './router'
+import App from './App.vue'
 new Vue({
-    el: '#app',
-    template: '<h1>hello</h1>'
+  el: '#app',
+  router,
+  components: {App},
+  template: '<App />'
 })
