25805a8fd6e582f35e6529e2f32f6690a43aaeb8
teruya <abas.trace.on@gmail.com>
2019-02-03 11:35:34 +0900

    add login implementation

--- routes/api.php
+++ routes/api.php
@@ -14,3 +14,4 @@ use Illuminate\Http\Request;
 */
 
 Route::post('/register', 'Auth\RegisterController@register')->name('register');
+Route::post('/login', 'Auth\LoginController@login')->name('login');
